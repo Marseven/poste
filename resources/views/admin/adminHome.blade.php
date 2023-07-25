19f4d9e170fabe8d3863a7b461b90b7594b88100
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-07-25 05:06:25 +0100

    debug home

--- resources/views/admin/adminHome.blade.php
+++ resources/views/admin/adminHome.blade.php
@@ -29,11 +29,11 @@
                                 <div class="flex">
                                     <i data-lucide="package" class="report-box__icon text-primary"></i>
                                     <!--div class="ml-auto">
-                                                                                                                                                                                                                            <div class="report-box__indicator bg-success tooltip cursor-pointer" title="33% Higher than last month"> 33% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
-                                                                                                                                                                                                                        </div-->
+                                                                                                                                                                                                                                    <div class="report-box__indicator bg-success tooltip cursor-pointer" title="33% Higher than last month"> 33% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
+                                                                                                                                                                                                                                </div-->
                                 </div>
                                 <div class="text-3xl font-medium leading-8 mt-6">
-                                    {{ $expeditions->count() }}
+                                    0
                                 </div>
                                 <div class="text-base text-slate-500 mt-1">Nbre d'expéditions</div>
                             </div>
@@ -45,11 +45,11 @@
                                 <div class="flex">
                                     <i data-lucide="package" class="report-box__icon text-pending"></i>
                                     <!--div class="ml-auto">
-                                                                                                                                                                                                                            <div class="report-box__indicator bg-danger tooltip cursor-pointer" title="2% Lower than last month"> 2% <i data-lucide="chevron-down" class="w-4 h-4 ml-0.5"></i> </div>
-                                                                                                                                                                                                                        </div-->
+                                                                                                                                                                                                                                    <div class="report-box__indicator bg-danger tooltip cursor-pointer" title="2% Lower than last month"> 2% <i data-lucide="chevron-down" class="w-4 h-4 ml-0.5"></i> </div>
+                                                                                                                                                                                                                                </div-->
                                 </div>
                                 <div class="text-3xl font-medium leading-8 mt-6">
-                                    {{ $paiements->count() }}
+                                    0
                                 </div>
                                 <div class="text-base text-slate-500 mt-1">Nbre d'expéditions en cours</div>
                             </div>
@@ -61,11 +61,11 @@
                                 <div class="flex">
                                     <i data-lucide="package" class="report-box__icon text-warning"></i>
                                     <!--div class="ml-auto">
-                                                                                                                                                                                                                            <div class="report-box__indicator bg-success tooltip cursor-pointer" title="12% Higher than last month"> 12% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
-                                                                                                                                                                                                                        </div-->
+                                                                                                                                                                                                                                    <div class="report-box__indicator bg-success tooltip cursor-pointer" title="12% Higher than last month"> 12% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
+                                                                                                                                                                                                                                </div-->
                                 </div>
                                 <div class="text-3xl font-medium leading-8 mt-6">
-                                    {{ $clients->count() }}
+                                    0
                                 </div>
                                 <div class="text-base text-slate-500 mt-1">Nbre d'expéditions livrées</div>
                             </div>
@@ -77,11 +77,11 @@
                                 <div class="flex">
                                     <i data-lucide="package" class="report-box__icon text-success"></i>
                                     <!--div class="ml-auto">
-                                                                                                                                                                                                                            <div class="report-box__indicator bg-success tooltip cursor-pointer" title="22% Higher than last month"> 22% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
-                                                                                                                                                                                                                        </div-->
+                                                                                                                                                                                                                                    <div class="report-box__indicator bg-success tooltip cursor-pointer" title="22% Higher than last month"> 22% <i data-lucide="chevron-up" class="w-4 h-4 ml-0.5"></i> </div>
+                                                                                                                                                                                                                                </div-->
                                 </div>
                                 <div class="text-3xl font-medium leading-8 mt-6">
-                                    {{ $packages->count() }}
+                                    {{ $expeditions->count() }}
                                 </div>
                                 <div class="text-base text-slate-500 mt-1">Nbre de réservations</div>
                             </div>
