950dc1a9db0a6f27fc83692e48b5bf61cc67d0cc
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-07-31 02:20:05 +0100

    debug Form

--- resources/views/admin/adminExpeditionList.blade.php
+++ resources/views/admin/adminExpeditionList.blade.php
@@ -198,7 +198,7 @@
                                                                 </div>
                                                             </div>
                                                         </div>
-
+                                                        <br><br>
                                                         <div id="direct-form" style="display: none">
                                                             <div class="col-span-12 sm:col-span-6">
                                                                 <label for="modal-form-1" class="form-label">Opérateur
@@ -355,11 +355,11 @@
             const id = $("#id").val();
 
             if ($(".paylink-" + id).val() != "link") {
-                direct.style.display = "none";
-                link.style.display = "block";
-            } else {
                 direct.style.display = "block";
                 link.style.display = "none";
+            } else {
+                direct.style.display = "none";
+                link.style.display = "block";
             }
         }
 
