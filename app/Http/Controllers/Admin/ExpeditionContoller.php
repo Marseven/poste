a7cb67c2951431e5e62904567ebb10ffae7b9e5a
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-07-31 18:23:50 +0100

    dbg 9

--- app/Http/Controllers/Admin/ExpeditionContoller.php
+++ app/Http/Controllers/Admin/ExpeditionContoller.php
@@ -273,9 +273,9 @@ class ExpeditionContoller extends Controller
         } else {
             if ($request->input('service') == 1) {
                 $price = PriceExpedition::where('type', 'Standard')->where('service_id', $request->input('service'))->where('zone_id', $request->input('zone'))->where('mode_id', $request->input('mode'))->first();
-                dd($price);
                 if ($request->poids > 0.5) {
                     if ($price && $price->first == 1) $price_sup = PriceExpedition::where('type', 'Supplémentaire')->where('service_id', $request->input('service'))->where('zone_id', $request->input('zone'))->where('mode_id', $request->input('mode'))->first();
+                    dd($price_sup);
                     $first = 0.5;
                     $last = $request->poids - $first;
                     $poids_sup = $last / $first;
