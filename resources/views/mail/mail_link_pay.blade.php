1578e0ab258a06e95d5f72d4ff9718fca964d38d
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-08-02 03:49:19 +0100

    dbg 1

--- resources/views/mail/mail_link_pay.blade.php
+++ resources/views/mail/mail_link_pay.blade.php
@@ -1,9 +1,9 @@
 @component('mail::message')
     <h1>Bonjour M./Mme. {{ $expedition->name_exp }},</h1>
 
-    Cliquez sur le bouton ci-dessous pour payer l'expédition N° {{ $expedition->reference }}.
+    Cliquez sur le lien ci-dessous pour payer l'expédition N° {{ $expedition->reference }}.
 
-    <a href="{{ $link }}" target="_blank">Payer l'expédition</a>
+    {{ $link }}
 
     Cordialement,
     La Poste Gaboanaise
