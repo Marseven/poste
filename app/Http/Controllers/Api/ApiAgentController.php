6380972b56a02ebdb5300730e224c419f08d6d97
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-08-02 02:45:39 +0100

    dbg 1

--- app/Http/Controllers/Api/ApiAgentController.php
+++ app/Http/Controllers/Api/ApiAgentController.php
@@ -1060,7 +1060,7 @@ class ApiAgentController extends Controller
             // Préparer la requete
             $suivi->code = $ext .  '-' . $code;
             $suivi->etape_id = $request->input('position');
-            $suivi->statut = $request->input('statut');
+            $suivi->status = $request->input('statut');
             $suivi->rapport = $request->input('rapport');
             $suivi->package_id = $request->input('package_id');
 
