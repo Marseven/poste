67f17317f8e1756976dffcfa45d5b73a85a54631
yannickaboh <yannickabohthierry@gmail.com>
2023-09-12 00:50:07 +0100

    Update PriceExpeditionResource.php
    Update PriceExpeditionResource.php

--- app/Http/Resources/PriceExpeditionResource.php
+++ app/Http/Resources/PriceExpeditionResource.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Resources;
 
+use App\Models\ModeExpedition;
+use App\Models\ServiceExpedition;
+use App\Models\Zone;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class PriceExpeditionResource extends JsonResource
 {
@@ -14,6 +18,37 @@ class PriceExpeditionResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // Set locale
+        Carbon::setLocale('fr');
+        
+        // Get relation data
+        $zone = Zone::find($this->zone_id);
+        $service = ServiceExpedition::find($this->service_id);
+        $mode = ModeExpedition::find($this->mode_id);
+
+        return [
+            'id' => $this->id,
+
+            'zone' => $zone ? $zone->libelle : 'Non defini',
+            'service' => $service ? $service->libelle : 'Non defini',
+            'mode' => $mode ? $mode->libelle : 'Non defini',
+
+            'code' => $this->code,
+            'libelle' => $this->libelle,
+
+            'zone_id' => $this->zone_id,
+            'service_id' => $this->service_id,
+            'mode_id' => $this->mode_id,
+
+            'type' => $this->type,
+            'first' => $this->first,
+
+            'weight' => $this->weight,
+            'price' => $this->price,
+
+            'active' => $this->active,
+            'created_at' => Carbon::parse($this->created_at)->translatedFormat('l jS F Y'),
+            'updated_at' => Carbon::parse($this->updated_at)->translatedFormat('l jS F Y'),
+        ];
     }
 }
