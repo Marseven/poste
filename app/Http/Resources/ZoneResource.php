c2e80f0e9cb4e49334c85b41409b4e408b5a44e3
yannickaboh <yannickabohthierry@gmail.com>
2023-09-11 21:17:14 +0100

    Update ZoneResource.php
    Update ZoneResource.php

--- app/Http/Resources/ZoneResource.php
+++ app/Http/Resources/ZoneResource.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Reseau;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class ZoneResource extends JsonResource
 {
@@ -14,6 +16,22 @@ class ZoneResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // Set locale
+        Carbon::setLocale('fr');
+        
+        // Get relation data
+        $reseau = Reseau::find($this->ville_id);
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'libelle' => $this->libelle,
+            'phone' => $this->phone,
+            'reseau_id' => $this->reseau_id,
+            'reseau' => $reseau ? $reseau->libelle : 'Non defini',
+
+            'active' => $this->active,
+            'created_at' => Carbon::parse($this->created_at)->translatedFormat('l jS F Y'),
+            'updated_at' => Carbon::parse($this->updated_at)->translatedFormat('l jS F Y'),
+        ];
     }
 }
