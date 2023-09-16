1b1363ae6c75fb222fa6ddf33c0ddf461a3c80f9
yannickaboh <yannickabohthierry@gmail.com>
2023-09-16 13:26:32 +0100

    Update OnesignalResource.php
    Update OnesignalResource.php
    return [
                'id' => $this->id,
                'player_id' => $this->player_id,
                'user_id' => $this->user_id,
                'utilisateur' => $utilisateur ? $utilisateur->name : 'Non defini',
                'role' => $utilisateur ? $utilisateur->role : 'Non defini',
    
                'active' => $this->active,
                'created_at' => Carbon::parse($this->created_at)->translatedFormat('l jS F Y'),
                'updated_at' => Carbon::parse($this->updated_at)->translatedFormat('l jS F Y'),
            ];

--- app/Http/Resources/OnesignalResource.php
+++ app/Http/Resources/OnesignalResource.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
 class OnesignalResource extends JsonResource
 {
@@ -14,6 +16,23 @@ class OnesignalResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // Set locale
+        Carbon::setLocale('fr');
+        
+        // Get relation data
+        $utilisateur = User::find($this->user_id);
+        
+
+        return [
+            'id' => $this->id,
+            'player_id' => $this->player_id,
+            'user_id' => $this->user_id,
+            'utilisateur' => $utilisateur ? $utilisateur->name : 'Non defini',
+            'role' => $utilisateur ? $utilisateur->role : 'Non defini',
+
+            'active' => $this->active,
+            'created_at' => Carbon::parse($this->created_at)->translatedFormat('l jS F Y'),
+            'updated_at' => Carbon::parse($this->updated_at)->translatedFormat('l jS F Y'),
+        ];
     }
 }
