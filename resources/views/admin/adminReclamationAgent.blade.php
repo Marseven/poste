4f9d5289ee1c7c3a96808e9120a1d08a85afafa7
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-09-17 21:19:05 +0100

    dbg 2

--- resources/views/admin/adminReclamationAgent.blade.php
+++ resources/views/admin/adminReclamationAgent.blade.php
@@ -213,7 +213,7 @@
 
                     </tbody>
                 </table>
-                @if ($reclamations->count() > 0)
+                @if ($reclamations->count() == 0)
                     <div class="flex items-center mb-2">
                         <div class="alert alert-pending show flex items-center mb-2" role="alert"> <i
                                 data-lucide="alert-triangle" class="w-6 h-6 mr-2"></i> Aucun élément pour le
